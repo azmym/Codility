2980946025605c6219903c08706289c6580ae673
Mahmoud Azmy <mahmoud.azmy@gmail.com>
2016-12-14 23:40:42 +0200

    Another Solution
    Solved OddOccurrencesInArray in Java

---
+++ OddOccurrencesInArray/Solution2.java
@@ -0,0 +1,16 @@
+public class Solution2 {
+
+	public static void main(String[] args) {
+		System.out.println(new Solution2().solution( new int []{9,3,9,3,9,7,9}));
+	}
+
+	public int solution(int[] A) {
+		if (A.length==1)
+            return A[0];
+        int result = 0;
+        for (int i=0; i<A.length; i++)
+        	result ^= A[i];
+        return result;
+	}
+
+}
